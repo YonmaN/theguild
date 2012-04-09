1c5004a7b9ae7c129630806b4784fb4831b34989
Yonni <Yonni@yonman>
2012-04-09 13:08:56 +0300

    Forgot to change schema file

--- data/theguild.sql
+++ data/theguild.sql
@@ -1,3 +1,2 @@
 CREATE TABLE "game" ("game_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "game_name" VARCHAR, "game_name_short" VARCHAR);
-CREATE TABLE "profile" ("profile_id" INTEGER PRIMARY KEY  NOT NULL ,"user_id" INTEGER NOT NULL ,"name" VARCHAR,"birth_date" DATETIME,"visible" BOOL,"lfg" INTEGER,"bio" TEXT,"humour" INTEGER,"mobility" INTEGER,"focus" INTEGER,"hospitality" INTEGER,"strictness" INTEGER, "gender" INTEGER);
 CREATE TABLE "user_game" ("user_id" INTEGER NOT NULL , "game_id" INTEGER NOT NULL , "comments" TEXT, "ability_crunch" INTEGER, "ability_lore" INTEGER, "ability_gm" INTEGER, "learn" BOOL, PRIMARY KEY ("user_id", "game_id"));
